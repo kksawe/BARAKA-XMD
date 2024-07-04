b1d479003aca470f1c3771e11707de8d0849d0e5
kksawe <161048906+kksawe@users.noreply.github.com>
2024-07-04 07:51:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,14 +10,14 @@ global.email = 'textnow1pak@gmail.com'
 global.github = 'https://github.com/naveeddogar/KING-MD'
 global.location = 'pakistan'
 global.gurl = 'https://instagram.com/naveeddogar_' // add your username
-global.sudo = process.env.SUDO || '923096566451'
+global.sudo = process.env.SUDO || '254797653096
 global.devs = '923096566451';
 global.website = 'https://github.com/naveeddogar/KING-MD' // Hello world
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/a6b9bbde7feaa92c69c7b.jpg'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'KING-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'Naveed Dogar' : process.env.OWNER_NAME,
-  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
+  ownername: process.env.OWNER_NAME === undefined ? 'Lofe Palmer' : process.env.OWNER_NAME,
+  sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYUxLSXVLemZnRGR3cGljNUJJeWRkZDc5ZVJGSTViMmkvZmtOcXZWMDlXUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYXpCYkxUZXNHMWtMdDNHV0xsLzUvaVBxQWVXWk5maEV6MVNod2FrWUp5Zz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFSzgvOWZXVFFFSS9aUDlCL09tL1NRWTk5aDJPRHJtVnhiOEhKeEZVUzI4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJrY1dJak5QNmNPb0lyUTRBVExrV1pSM1hldzBES1Z0ckpZa290SkJwM2lRPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IktJdWgzMzJsYWdCbkdDOUtGSm5JU1BKUGVFdjVleDMxK1BoWHFRTlVHSEk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ikw4N0RjMjNUcE1qMmQvZ01jcGNYY1R5bExwbFFwanFDeWF6UkJmbWM1eWM9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUZjck9lQkxWd3pldXJ5Q2NoS2xUNkRBdUhZMW5ZTnJNeDcrUDAzQURtZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMU0yUVVjbENJcXE5KzhDd3BiWURtdmk0OUFLaUYxaVp5Q2Ztbk9qZk1UQT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjRlajhnb2xXNjJkd29sYmFjNk56K3p3LzdGQzB5WjQ0NTNMYzk4VHhTbTJsVm9VaWlOTFBkcEsrNTFKTlYrNUQ4ay9Jd0FCQjlybU9RbmxDZnFUUGp3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTk3LCJhZHZTZWNyZXRLZXkiOiJCdFhROGNnR3lHSzZJeU9EcU56NldadEthc05YVUVPb0JWNjVFTEhvdHZvPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJnY0t6cVdNa1N0V210TThDZk9vdFRBIiwicGhvbmVJZCI6ImJhYzgxMzFiLTlmYjktNDE2OS1iZmE0LTg0OGEwZDc4NjUyMyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIvcWZNL0VsRGhMeUd6OExmYkpMbVBiUUN2NWs9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibXg5MU1CN1A2S0tiejhybGNkbkRQNWhtV1c0PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlZERDVHQVoyIiwibWUiOnsiaWQiOiIyNTQ3OTc2NTMwOTY6MTBAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0t6ZjlaMEhFTWF1OUxNR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6Iks0aWFzVVo0M09aN2RDTkRJMndLK3A3QmdFZldzTkdOUzdvSExCUU5EMFU9IiwiYWNjb3VudFNpZ25hdHVyZSI6ImE4QW94MHl1bWZqZDArU1VGUE9oQ2wrMDlSZjdvdzNmaUhEKzNGSXlJNXBNY0dlNDJPdEVQYlc0Tzd1ZFR0aGJaazYvT2xuSGtHSnBBSUtuaG8xakNRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJncnFmZkVobnVNRU9Oc2VFTjc3QTBlakFGb2JVU0NYYTgwL01OckxUOGo4dW45MC9kSTNjN1ZDNElnQ3dWcmQwbjdvelIvWGdqeUFQb1ZwWXFCd29pQT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI1NDc5NzY1MzA5NjoxMEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJTdUltckZHZU56bWUzUWpReU5zQ3ZxZXdZQkgxckRSalV1NkJ5d1VEUTlGIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzE5NDc0MDAzfQ==
   author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'Naveed-Dogar' : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS  || 'true' ,
   packname:  process.env.PACK_NAME.split(";")[1] === undefined ? 'King-Md' : process.env.PACK_NAME.split(";")[1],
@@ -45,7 +45,7 @@ module.exports = {
 },
   VERSION: process.env.VERSION === undefined ? 'Ｖ-1.０.１' : process.env.VERSION,
   LANG: process.env.THEME|| 'SECKTOR-2.0',
-  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
 
 
